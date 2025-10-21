db2173595d25361a67a3c16ac5682454b4453665
Martin08ABM <martin08abm@gmail.com>
2025-10-21 20:40:07 +0200

    Primer commit de WhatIf AI

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@fonts': new URL('./public/fonts/', import.meta.url).pathname
+      }
+    },
+  }
+});
