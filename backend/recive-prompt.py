e48aeab9e588a4b930801da4d28df4981937a218
Martin08ABM <martin08abm@gmail.com>
2025-11-06 21:23:33 +0100

    Ajustes para el despliegue en Render

--- backend/recive-prompt.py
+++ backend/recive-prompt.py
@@ -82,4 +82,4 @@ async def continuar_historia(request_data: PromptRequest):
 
 if __name__ == "__main__":
     # Comando para iniciar el servidor Uvicorn que mantiene la API "escuchando"
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
